f7cd0e88a1b989c4c6dc2f304d93f48e6eea131a
trapd00r <trapd00r@trapd00r.se>
2010-04-11 11:07:03 +0200

    Added XOSD support

--- mpdmon-daemon.pl
+++ mpdmon-daemon.pl
@@ -10,8 +10,13 @@ use Proc::Daemon;
 use Audio::MPD;
 
 my $mpd = Audio::MPD->new;
-my $cmd = "stumpish echo "; #change this to whatever you want
-sub monitor {
+# arbitary command to run on song change
+#my $cmd = "stumpish echo "; #stumpwm specific
+# for xosd
+my $xosd_font = '-*-profont-*-*-*-*-15-*-*-*-*-*-*-*';
+my $cmd = "osd_cat -f $xosd_font -A right -p bottom -c \"#a8ff00\" -s 3";
+
+sub monitor {  
   my $np = "";
   print "Daemonizing...\n";
   Proc::Daemon::Init;
@@ -22,7 +27,8 @@ sub monitor {
 
     if("$np" ne "$current") {
       $np = $current;
-      system("$cmd \"$output\"");
+      #system("$cmd \"$output\""); # for stumpish
+      system("printf \"$output\"|$cmd"); # for xosd
     }
     sleep 2;
   }
